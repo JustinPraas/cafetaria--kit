ff0a3f0fb4905d27bfb29f87acd515d01e14047a
Justin Praas <jw.praas2@gmail.com>
2021-10-02 18:49:23 +0200

    Initial commit

---
+++ cms/src/main/java/nl/praas/cafetariasolution/fp/cms/CmsApplication.java
@@ -0,0 +1,13 @@
+package nl.praas.cafetariasolution.fp.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsApplication.class, args);
+	}
+
+}
